e073a0d189462a02583668d87f0d4ac4eb3e2b30
KateNegrienko <katya@negrienko.com>
2017-03-30 13:44:48 +0300

    change colors

--- samples/index.js
+++ samples/index.js
@@ -31,72 +31,72 @@ app.controller('appController',
                         link: "http://webui.pipdevs.com/pip-webui-css/index.html"
                     },{
                         name: 'Basic controls',
-                        color: 5,
+                        color: 0,
                         link: "http://webui.pipdevs.com/pip-webui-controls/index.html"
                     },
                     {
                         name: 'Dates',
-                        color: 6,
+                        color: 3,
                         link: "http://webui.pipdevs.com/pip-webui-dates/index.html"
                     },{
                         name: 'Dialogs',
-                        color: 7,
+                        color: 4,
                         link: "http://webui.pipdevs.com/pip-webui-dialogs/index.html"
                     },
                     {
                         name: 'Errors',
-                        color: 8,
+                        color: 1,
                         link: "http://webui.pipdevs.com/pip-webui-errors/index.html"
                     },{
                         name: 'Files',
-                        color: 9,
+                        color: 2,
                         link: "http://webui.pipdevs.com/pip-webui-files/index.html"
                     },
                     {
                         name: 'Headers',
-                        color: 10,
+                        color: 3,
                         link: "http://webui.pipdevs.com/pip-webui-headers/index.html"
                     },{
                         name: 'Help',
-                        color: 11,
+                        color: 0,
                         link: "http://webui.pipdevs.com/pip-webui-help/index.html"
                     },
                     {
                         name: 'Landing',
-                        color: 12,
+                        color: 2,
                         link: "http://webui.pipdevs.com/pip-webui-landing/index.html"
                     },
                     {
                         name: 'Layouts',
-                        color: 13,
+                        color: 3,
                         link: "http://webui.pipdevs.com/pip-webui-layouts/index.html"
                     },{
                         name: 'Lists',
-                        color: 14,
+                        color: 4,
                         link: "http://webui.pipdevs.com/pip-webui-lists/index.html"
                     },
                     {
                         name: 'Locations',
-                        color: 15,
+                        color: 2,
                         link: "http://webui.pipdevs.com/pip-webui-locations/index.html"
                     },{
                         name: 'Nav',
-                        color: 7,
+                        color: 1,
                         link: "http://webui.pipdevs.com/pip-webui-nav/index.html"
                     },
                     {
                         name: 'Services',
-                        color: 9,
+                        color: 0,
                         link: "http://webui.pipdevs.com/pip-webui-services/index.html"
                     },
                     {
                         name: 'Settings',
-                        color: 5,
+                        color: 2,
                         link: "http://webui.pipdevs.com/pip-webui-settings/index.html"
                     },
                     {
                         name: 'Themes',
-                        color: 6,
+                        color: 0,
                         link: "http://webui.pipdevs.com/pip-webui-themes/index.html"
                     }
                 ]
@@ -106,22 +106,22 @@ app.controller('appController',
                 submodules: [
                     {
                         name: 'Angular Material Intro',
-                        color: 15,
+                        color: 0,
                         link: "https://material.angularjs.org/latest/#/"
                     },
                     {
                         name: 'Angular Material GitHub',
-                        color: 14,
+                        color: 4,
                         link: "https://github.com/angular/material"
                     },
                     {
                         name: 'Material Design Guidelines',
-                        color: 13,
+                        color: 3,
                         link: "https://www.google.com/design/spec/material-design/introduction.html"
                     },
                     {
                         name: 'Google Design References',
-                        color: 12,
+                        color: 2,
                         link: "http://design.google.com"
                     }
                 ]
